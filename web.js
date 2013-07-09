f8bb2c1f2abc76bb4e6b6c099d03c5d8d632e19e
Kidzik <kidzik@kidzik-X71SL.(none)>
2013-07-08 21:35:30 -0600

    db support, menu, submit a joke, almost 1.0 version

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger())
-  , fs = require('fs');
+  , fs = require('fs')
+  , nStore = require('nstore');
+
+var jokes = nStore.new('data/jokes.db', function () {});
+
+function err(err) {
+    if (err) { throw err; }
+}
+
+jokes.save(1, {content: "A suicide bomber walks into a bar.\nBartender says \"We don't serve suicide bombers here\","}, err);
+jokes.save(2, {content: "A second joke,"}, err);
 
 app.get('/', function(request, response) {
-//  response.send('Hello World2!');
 	fs.readFile(__dirname + '/html/index.html',
 	function (err, data) {
 		if (err) {
@@ -17,7 +26,16 @@ app.get('/', function(request, response) {
 	});
 });
 
+app.get('/joke/:id/', function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	jokes.get(parseInt(request.params.id), function (err, doc, meta) {
+		response.end(doc['content']);
+	})
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
