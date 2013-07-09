3404b30bf7408e4f33d400e64282cecb53f4397d
Kidzik <kidzik@kidzik-X71SL.(none)>
2013-07-08 19:54:54 -0600

    first version

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger())
+  , fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+	fs.readFile(__dirname + '/html/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+
+		response.writeHead(200);
+		response.end(data);
+	});
 });
 
 var port = process.env.PORT || 5000;
